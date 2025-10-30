dd16b42a219c54a7db790c2394ceb2f5c0cafccc
Swinal Waghmare <swinalwaghmare28@gmail.com>
2025-10-30 18:23:15 +0530

    dyanamodb lock added

--- [04]-state-locking/backend.tf
+++ [04]-state-locking/backend.tf
@@ -2,7 +2,9 @@ terraform {
   backend "s3" {
     bucket = "mlops-swinal"
     key = "terraform/terraform.tfstate"
-    use_lockfile = true # to use s3 native locking 1.19 version above
+    # use_lockfile = true # to use s3 native locking 1.19 version above
     region = "us-east-1"
+    dynamodb_table = "swinal"
+    encrypt = true
   }
 }
