7545cf0026ef87d074c8502d83f40c1ade805add
Swinal Waghmare <swinalwaghmare28@gmail.com>
2025-10-30 17:28:48 +0530

    developer-1 lockfile added

--- [04]-state-locking/backend.tf
+++ [04]-state-locking/backend.tf
@@ -2,6 +2,7 @@ terraform {
   backend "s3" {
     bucket = "mlops-swinal"
     key = "terraform/terraform.tfstate"
+    use_lockfile = true # to use s3 native locking 1.19 version above
     region = "us-east-1"
   }
 }
--- [04]-state-locking/main.tf
+++ [04]-state-locking/main.tf
@@ -4,3 +4,21 @@ resource "aws_vpc" "name" {
       Name = "TF-VPC-Developer2"
     }
 }
+
+resource "aws_subnet" "name" {
+    vpc_id = aws_vpc.name.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "us-east-1a"
+    tags = {
+        Name = "TF-Subnet-1"
+  }
+}
+
+resource "aws_instance" "name" {
+    ami = "ami-0bdd88bd06d16ba03"
+    instance_type = "t2.micro"
+    subnet_id = aws_subnet.name.id
+    tags = {
+      Name = "Developer-1-Instance"
+    }
+}
