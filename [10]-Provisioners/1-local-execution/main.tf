09ab6e016b147365037c18ee4447fb21bb4bedbf
Swinalwaghmare <swinalwaghmare28.com>
2025-11-10 17:23:28 +0530

    feat: provision EC2 instance with SSH access and local-exec provisioner
    Implemented Terraform configuration for basic EC2 deployment within a custom VPC:
    - Created VPC (10.0.0.0/16) tagged as 'TF-VPC' for network isolation.
    - Defined public subnet (10.0.1.0/24) in availability zone us-east-1a.
    - Configured security group 'EC2-Connection' to allow inbound SSH (port 22) and unrestricted outbound traffic.
    - Launched EC2 instance (t2.micro) using AMI ami-0157af9aea2eef346 and key pair 'spider'.
    - Enabled public IP association for external connectivity.
    - Added local-exec provisioner to log instance creation ID to output.txt after deployment.

---
+++ [10]-Provisioners/1-local-execution/main.tf
@@ -0,0 +1,57 @@
+# 
+provider "aws" {
+  
+}
+
+resource "aws_vpc" "name" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+      Name = "TF-VPC"
+    }
+}
+
+resource "aws_subnet" "public" {
+    vpc_id = aws_vpc.name.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "us-east-1a"
+    tags = {
+      Name = "Public-Subnet-1"
+    }
+}
+
+resource "aws_security_group" "name" {
+    name = "EC2-Connection"
+    description = "Allow SSH Connection"
+    vpc_id = aws_vpc.name.id
+    tags = {
+      Name = "SSH"
+    }
+    ingress{
+        description = "SSH for all"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress{
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+  
+}
+
+resource "aws_instance" "ec2" {
+  ami = "ami-0157af9aea2eef346"
+  instance_type = "t2.micro"
+  key_name = "spider"
+  vpc_security_group_ids = [aws_security_group.name.id]
+  subnet_id = aws_subnet.public.id
+  associate_public_ip_address = true
+
+
+  provisioner "local-exec" {
+    command = "echo instance ${self.id} created >> output.txt"
+  }
+}
